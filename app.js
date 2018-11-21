239a992eb3c350b75480de49b9faed3dba4b67d9
Ryan Leach <ryanleach@Ryans-MacBook-Pro.local>
2018-11-21 10:23:23 -0700

    grid created

---
+++ app.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function(){
+    var canvasElement = document.getElementsById("canvas")   
+    canvasElement.addEventListener("click", function(butts){
+        console.log(canvasElement)
+})
+
+
+
+
+
+
+
+
+    // var flashlight = document.getElementById("flashlight")
+    // flashlight.addEventListener("click", function(){
+    //     flashlight.classList.toggle('on')
+    //     flashlight.classList.toggle('off')
+    // })
