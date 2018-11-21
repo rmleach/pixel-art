11a0ce6ec35a43b6809ab9e79d58d3edc3687769
Ryan Leach <ryanleach@Ryans-MacBook-Pro.local>
2018-11-21 14:46:12 -0700

    reformatted css from bootstrap to flexbox

--- app.js
+++ app.js
@@ -1,15 +1,100 @@
 document.addEventListener("DOMContentLoaded", function() {
     var pixel1 = document.getElementById("1")
+    var pixel2 = document.getElementById("2")
+    var pixel3 = document.getElementById("3")
+    var pixel4 = document.getElementById("4")
+    var pixel5 = document.getElementById("5")
+    var pixel6 = document.getElementById("6")
+    var pixel7 = document.getElementById("7")
+    var pixel8 = document.getElementById("8")
+    var pixel9 = document.getElementById("9")
+    var pixel10 = document.getElementById("10")
+    var pixel11 = document.getElementById("11")
+    var pixel12 = document.getElementById("12")
+    var pixel13 = document.getElementById("13")
+    var pixel14 = document.getElementById("14")
+    var pixel15 = document.getElementById("15")
+    var pixel16 = document.getElementById("16")
     var red = document.getElementById("red")
-    let color = ""
+    var yellow = document.getElementById("yellow")
+    var green = document.getElementById("green")
+    var blue = document.getElementById("blue")
+    let color = "red"
     red.addEventListener("click", function(){
         color = "red"
+    })
+    yellow.addEventListener("click", function(){
+        color = "yellow"
+    })
+    green.addEventListener("click", function(){
+        color = "green"
+    })
+    blue.addEventListener("click", function(){
+        color = "blue"
     })
     pixel1.addEventListener("click", function(){
         pixel1.classList.toggle(color)
         pixel1.classList.toggle("pixel")
-        console.log(pixel1)
-
+    })
+    pixel2.addEventListener("click", function(){
+        pixel1.classList.toggle(color)
+        pixel1.classList.toggle("pixel")
+    })
+    pixel3.addEventListener("click", function(){
+        pixel1.classList.toggle(color)
+        pixel1.classList.toggle("pixel")
+    })
+    pixel4.addEventListener("click", function(){
+        pixel1.classList.toggle(color)
+        pixel1.classList.toggle("pixel")
+    })
+    pixel5.addEventListener("click", function(){
+        pixel1.classList.toggle(color)
+        pixel1.classList.toggle("pixel")
+    })
+    pixel6.addEventListener("click", function(){
+        pixel1.classList.toggle(color)
+        pixel1.classList.toggle("pixel")
+    })
+    pixel7.addEventListener("click", function(){
+        pixel1.classList.toggle(color)
+        pixel1.classList.toggle("pixel")
+    })
+    pixel8.addEventListener("click", function(){
+        pixel1.classList.toggle(color)
+        pixel1.classList.toggle("pixel")
+    })
+    pixel9.addEventListener("click", function(){
+        pixel1.classList.toggle(color)
+        pixel1.classList.toggle("pixel")
+    })
+    pixel10.addEventListener("click", function(){
+        pixel1.classList.toggle(color)
+        pixel1.classList.toggle("pixel")
+    })
+    pixel11.addEventListener("click", function(){
+        pixel1.classList.toggle(color)
+        pixel1.classList.toggle("pixel")
+    })
+    pixel12.addEventListener("click", function(){
+        pixel1.classList.toggle(color)
+        pixel1.classList.toggle("pixel")
+    })
+    pixel13.addEventListener("click", function(){
+        pixel1.classList.toggle(color)
+        pixel1.classList.toggle("pixel")
+    })
+    pixel14.addEventListener("click", function(){
+        pixel1.classList.toggle(color)
+        pixel1.classList.toggle("pixel")
+    })
+    pixel15.addEventListener("click", function(){
+        pixel1.classList.toggle(color)
+        pixel1.classList.toggle("pixel")
+    })
+    pixel16.addEventListener("click", function(){
+        pixel1.classList.toggle(color)
+        pixel1.classList.toggle("pixel")
     })
 })
 
