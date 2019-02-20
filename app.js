7ac054d6f90b86af4d1737fa8770ee043c86dd62
Ryan Leach <ryanleach@Ryans-MacBook-Pro.local>
2019-02-19 17:12:51 -0700

    refactoring project.

--- app.js
+++ app.js
@@ -1,106 +1,30 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var pixel1 = document.getElementById("1")
-    var pixel2 = document.getElementById("2")
-    var pixel3 = document.getElementById("3")
-    var pixel4 = document.getElementById("4")
-    var pixel5 = document.getElementById("5")
-    var pixel6 = document.getElementById("6")
-    var pixel7 = document.getElementById("7")
-    var pixel8 = document.getElementById("8")
-    var pixel9 = document.getElementById("9")
-    var pixel10 = document.getElementById("10")
-    var pixel11 = document.getElementById("11")
-    var pixel12 = document.getElementById("12")
-    var pixel13 = document.getElementById("13")
-    var pixel14 = document.getElementById("14")
-    var pixel15 = document.getElementById("15")
-    var pixel16 = document.getElementById("16")
-    var red = document.getElementById("red")
-    var yellow = document.getElementById("yellow")
-    var orchid = document.getElementById("orchid")
-    var blue = document.getElementById("blue")
-    let color = "red"
-    red.addEventListener("click", function(){
-        color = "red"
-    })
-    yellow.addEventListener("click", function(){
-        color = "yellow"
-    })
-    orchid.addEventListener("click", function(){
-        color = "orchid"
-    })
-    blue.addEventListener("click", function(){
-        color = "blue"
-    })
-    pixel1.addEventListener("click", function(){
-        pixel1.classList.toggle(color)
-        pixel1.classList.toggle("pixel")
-    })
-    pixel2.addEventListener("click", function(){
-        pixel2.classList.toggle(color)
-        pixel2.classList.toggle("pixel")
-    })
-    pixel3.addEventListener("click", function(){
-        pixel3.classList.toggle(color)
-        pixel3.classList.toggle("pixel")
-    })
-    pixel4.addEventListener("click", function(){
-        pixel4.classList.toggle(color)
-        pixel4.classList.toggle("pixel")
-    })
-    pixel5.addEventListener("click", function(){
-        pixel5.classList.toggle(color)
-        pixel5.classList.toggle("pixel")
-    })
-    pixel6.addEventListener("click", function(){
-        pixel6.classList.toggle(color)
-        pixel6.classList.toggle("pixel")
-    })
-    pixel7.addEventListener("click", function(){
-        pixel7.classList.toggle(color)
-        pixel7.classList.toggle("pixel")
-    })
-    pixel8.addEventListener("click", function(){
-        pixel8.classList.toggle(color)
-        pixel8.classList.toggle("pixel")
-    })
-    pixel9.addEventListener("click", function(){
-        pixel9.classList.toggle(color)
-        pixel9.classList.toggle("pixel")
-    })
-    pixel10.addEventListener("click", function(){
-        pixel10.classList.toggle(color)
-        pixel10.classList.toggle("pixel")
-    })
-    pixel11.addEventListener("click", function(){
-        pixel11.classList.toggle(color)
-        pixel11.classList.toggle("pixel")
-    })
-    pixel12.addEventListener("click", function(){
-        pixel12.classList.toggle(color)
-        pixel12.classList.toggle("pixel")
-    })
-    pixel13.addEventListener("click", function(){
-        pixel13.classList.toggle(color)
-        pixel13.classList.toggle("pixel")
-    })
-    pixel14.addEventListener("click", function(){
-        pixel14.classList.toggle(color)
-        pixel14.classList.toggle("pixel")
-    })
-    pixel15.addEventListener("click", function(){
-        pixel15.classList.toggle(color)
-        pixel15.classList.toggle("pixel")
-    })
-    pixel16.addEventListener("click", function(){
-        pixel16.classList.toggle(color)
-        pixel16.classList.toggle("pixel")
-    })
-})
+const pixelCanvas = document.querySelector('.canvas')
 
+const makeGrid = () => {
+  for(let i = 1; i <= 25; i++){
+    let gridRow = document.createElement('tr');
+    pixelCanvas.appendChild(gridRow);
+    for (let j = 1; j <= 25; j++) {
+      let gridCell = document.createElement('td');
+      gridRow.appendChild(gridCell);
+    }
+  }
+}
 
+const colorPicker = () => {
+  let input = document.querySelector("input")
+  for(let i = 1; i < input.lenght; i++){
+    input[i].addEventlistener("input", () =>{
+      red = document.querySelector("red").value,
+      green = document.querySelector("green").value,
+      blue = document.querySelector("blue").value 
+      display = document.querySelector("display")
+      console.log(display.style.background = "rgb(" + red + ", " + green + ", " + blue + ")")
 
+    })
+  }
+}
 
-
-
+makeGrid()
+colorPicker()
 
